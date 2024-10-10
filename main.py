b1917ae6e1e94cbfb7dded42ec597e14fca8a991
lenny <leopoldolenny@gmail.com>
2024-10-10 15:58:24 -0300

    Cuil calculator descarga cuil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, after_this_request
+
 from cuilCalculator import cuilCalculator
+import os
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -30,6 +32,37 @@ def index():
                     error = str(e)
 
     return render_template("index.html", cuil=cuil, error=error)
+
+
+import tempfile
+import shutil
+
+@app.route("/descargar_cuil/<cuil>")
+def descargar_cuil(cuil):
+    # Crea el contenido del archivo
+    contenido = f"Cuil: {cuil}\n"
+    
+    # Crea un archivo temporal
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.txt') as temp_file:
+        temp_file.write(contenido.encode('utf-8'))
+        temp_file_path = temp_file.name  # Guarda la ruta del archivo temporal
+    
+    # Envía el archivo para su descarga
+    response = send_file(temp_file_path, as_attachment=True)
+    
+    # Elimina el archivo después de enviarlo
+    @after_this_request
+    def cleanup(response):
+        try:
+            os.remove(temp_file_path)
+        except Exception as e:
+            print(f'Error al eliminar el archivo: {e}')
+        return response
+
+    return response
+
+
+
 
 
 
